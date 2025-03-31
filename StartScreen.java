774e55c1b980817ce2e051bb188832c532322c00
ricardo <ricardo.cmlee@gmail.com>
2025-03-31 08:56:16 -0400

    Resize screen size and imported Label class

--- StartScreen.java
+++ StartScreen.java
@@ -16,6 +16,6 @@ public class StartScreen extends World
     public StartScreen()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
+        super(1024, 800, 1); 
     }
 }
