604c4031a44718020bbb2add184f90aca13b97b8
ricardo <ricardo.cmlee@gmail.com>
2025-04-03 12:59:41 -0400

    Implemented Animator class and initialized WorldEffects

---
+++ WorldEffects.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class WorldEffects here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public abstract class WorldEffects extends Actor
+{   
+    protected boolean status;
+    
+    public abstract void start();
+    
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
