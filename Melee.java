a8416188d60ed7540c4349c8574762f307261bc8
ricardo <ricardo.cmlee@gmail.com>
2025-04-07 19:43:08 -0400

    Initalized character classes

---
+++ Melee.java
@@ -0,0 +1,27 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Melee here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Melee extends Character
+{
+    /**
+     * Act - do whatever the Melee wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public Melee(int age, int direction, int hp) {
+        super(age, direction, hp);
+    }
+    
+    public void act()
+    {
+        // Add your action code here.
+    }
+    
+    public void attack() {
+        
+    }
+}
