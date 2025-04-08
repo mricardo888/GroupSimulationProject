015bae9bb9ccaa6e36bfc5621aac94fa7c724391
1MarkHuang1 <350092060@gapps.yrdsb.ca>
2025-04-08 09:29:10 -0400

    Finished plus minus button
    -Added 1 out 4 buttons
     -Finished gold1
     - Still have gold2, xp1, xp2

---
+++ MinBut.java
@@ -0,0 +1,71 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class MinBut here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class MinBut extends Buttons
+{
+    private int g1;
+    private int g2;
+    private int xp1;
+    private int xp2;
+    private int which;
+    
+    public MinBut(GreenfootImage e, int x){
+        super(e);
+        which = x;
+    }
+    
+    public void act()
+    {
+        which();
+    }
+    
+    
+    public void minGold1(){
+        Settings s = (Settings)getWorld();
+        g1 = s.getGold1();
+        s.setGold1(g1 - 1);
+    }
+    public void minGold2(){
+        Settings s = (Settings)getWorld();
+        g2 = s.getGold2();
+        s.setGold1(g2 - 1);
+    }
+    public void minXp1(){
+        Settings s = (Settings)getWorld();
+        xp1 = s.getXp1();
+        s.setGold1(xp1 - 1);
+    }
+    public void minXp2(){
+        Settings s = (Settings)getWorld();
+        xp2 = s.getXp2();
+        s.setGold1(xp2 - 1);
+    }
+    
+    public void which(){
+        if (Greenfoot.mouseClicked(this)){
+            if (which == 1){
+                minGold1();
+            }
+            else if(which == 2){
+                minGold2();
+            }
+            else if(which == 3){
+                minXp1();
+            }
+            else{
+                minXp2();
+            }
+        }
+    }
+    
+    
+    
+    
+    
+    
+}
--- Settings.java
+++ Settings.java
@@ -4,17 +4,23 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class Settings extends World
 {
     //variables
-    private int gold1;
-    private int gold2;
-    private int xp1;
-    private int xp2;
+    private static int gold1;
+    private static int gold2;
+    private static int xp1;
+    private static int xp2;
     
+    
+    //Label
+    Label g1 = new Label(0,40);
     
     //Images
     GreenfootImage gold = new GreenfootImage("gold.png");
     GreenfootImage xp = new GreenfootImage("xp.png");
     GreenfootImage back = new GreenfootImage("back.png");
     GreenfootImage settin = new GreenfootImage("setting.png");
+    GreenfootImage plus = new GreenfootImage("plus.png");
+    GreenfootImage min = new GreenfootImage("min.png");
+
 
 
     
@@ -25,19 +31,31 @@ public class Settings extends World
         super(1024, 800,1);
         
         
+        
+        
+        
         //Image resize
         back.scale((int)(back.getWidth() * 0.8), (int)(back.getHeight() * 0.8));
         gold.scale((int)(gold.getWidth() * 0.3), (int)(gold.getHeight() * 0.3));
         xp.scale((int)(xp.getWidth() * 0.3), (int)(xp.getHeight() * 0.3));
         settin.scale((int)(settin.getWidth() * 1.4), (int)(settin.getHeight() * 1.4));
+        plus.scale((int)(plus.getWidth() * 0.2), (int)(plus.getHeight() * 0.2));
+        min.scale((int)(min.getWidth() * 0.2), (int)(min.getHeight() * 0.2));
 
 
-
-        
+    
+        //Label
+        addObject(g1,330,550);
         
         //Button Back to StartScreen
         BackButton backButton = new BackButton(back);
         addObject(backButton, getWidth()/2, 700);
+        
+        
+        PlusBut pg1But = new PlusBut(plus, 1);
+        addObject(pg1But,300,600);
+        MinBut mg1But = new MinBut(min, 1);
+        addObject(mg1But,360,600);
         
         getBackground().drawImage(gold,110,450);
         getBackground().drawImage(xp,110,250);
@@ -47,7 +65,7 @@ public class Settings extends World
     }
     
     public void act(){
-        
+        g1.setValue(gold1);
     }
     
     //Getters && setters
@@ -75,5 +93,17 @@ public class Settings extends World
     public void setXp2(int num){
         xp2 = num;
     }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
 }
