3226eb69e1f48a30316c5c4ad8458f27969c6d31
1MarkHuang1 <350092060@gapps.yrdsb.ca>
2025-04-15 09:05:20 -0400

    Added a useless thing doesnt matter

--- PlusBut.java
+++ PlusBut.java
@@ -8,7 +8,7 @@ public class PlusBut extends Buttons
     private int xp1;
     private int xp2;
     private int which;
-    
+    private boolean isPressed = false;
 
     
     public PlusBut(GreenfootImage e, int x){
@@ -19,31 +19,44 @@ public class PlusBut extends Buttons
     
     public void plusGold1(){
         Settings s = (Settings)getWorld();
-        g1 = s.getGold1();
-        s.setGold1(g1 + 10);
+        if(isPressed){
+            g1 = s.getGold1();
+            s.setGold1(g1 + 10);
+        }
+        
     }
     public void plusGold2(){
         Settings s = (Settings)getWorld();
-        g2 = s.getGold2();
-        s.setGold2(g2 + 10);
+        if(isPressed){
+            g2 = s.getGold2();
+            s.setGold2(g2 + 10);
+        }
+        
 
     }
     public void plusXp1(){
         Settings s = (Settings)getWorld();
-        xp1 = s.getXp1();
-        s.setXp1(xp1 + 10);
+        if(isPressed){
+            xp1 = s.getXp1();
+            s.setXp1(xp1 + 10);
+        }
+        
 
     }
     public void plusXp2(){
         Settings s = (Settings)getWorld();
-        xp2 = s.getXp2();
-        s.setXp2(xp2 + 10);
+        if(isPressed){
+            xp2 = s.getXp2();
+            s.setXp2(xp2 + 10);
+        }
+        
 
     }
     
     
     public void which(){
         if (Greenfoot.mouseClicked(this)){
+            isPressed = true;
             if (which == 1){
                 plusGold1();
             }
@@ -56,6 +69,10 @@ public class PlusBut extends Buttons
             else{
                 plusXp2();
             }
+        }
+        
+        if (Greenfoot.mouseDragEnded(this) || Greenfoot.mouseClicked(null)) {
+            isPressed = false;
         }
     }
     
